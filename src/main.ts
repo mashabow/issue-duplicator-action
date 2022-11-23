5fb0ce3046e5e04040bbd7a3970d9b0c36a21c7d
Masaya Nakamura <mashabow@shiromoji.net>
2022-11-23 22:21:50 +0900

    feat: get context

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.info(JSON.stringify(github.context, null, 2))
+    core.debug('debug!')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
