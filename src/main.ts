1d2c866cae9d8cb00523a2d48495dd904c43961f
Masaya Nakamura <mashabow@shiromoji.net>
2022-11-23 23:45:07 +0900

    feat: check trigger event and activity type

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
+    if (github.context.eventName !== 'issue_comment') {
+      throw new Error('This action must be used with `issue_comment` event.')
+    }
+
+    const payload = github.context.payload
+    if (payload.action !== 'created') {
+      throw new Error('This action must be used with `created` activity type.')
+    }
+
     core.info(JSON.stringify(github.context, null, 2))
     core.debug('debug!')
   } catch (error) {
