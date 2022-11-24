803434a95fb0b40e207a774dd30f316e9ba15509
Masaya Nakamura <mashabow@shiromoji.net>
2022-11-24 23:00:38 +0900

    build: generate GraphQL operation types

--- codegen.ts
+++ codegen.ts
@@ -1,14 +1,14 @@
-
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type {CodegenConfig} from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://docs.github.com/public/schema.docs.graphql",
+  schema: 'https://docs.github.com/public/schema.docs.graphql',
+  documents: 'src/**/*.graphql',
   generates: {
-    "src/graphql.ts": {
-      plugins: ["typescript"]
+    'src/graphql.ts': {
+      plugins: ['typescript', 'typescript-operations']
     }
   }
-};
+}
 
-export default config;
+export default config
