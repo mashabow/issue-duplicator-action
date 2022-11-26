731a6ec1948ed65e88e7cb5a46a17d1f72c94605
Masaya Nakamura <mashabow@shiromoji.net>
2022-11-27 00:49:42 +0900

    refactor: extract GitHub API calls to `ApiClient` class

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import {ProjectV2FieldValue, getSdk} from './graphql'
+import {ApiClient} from './api-client'
 // eslint-disable-next-line import/no-unresolved
 import {IssueCommentEvent} from '@octokit/webhooks-types'
 
@@ -21,89 +21,27 @@ async function run(): Promise<void> {
     if (event.comment.body.trim() !== '/duplicate') return
 
     const token = core.getInput('github-token')
-    const octokit = github.getOctokit(token)
+    const apiClient = new ApiClient(token)
 
-    // https://docs.github.com/en/rest/issues/issues#create-an-issue
-    const {data: createdIssue} = await octokit.rest.issues.create({
-      owner: event.repository.owner.login,
-      repo: event.repository.name,
-      title: event.issue.title,
-      body: event.issue.body ?? undefined,
-      milestone: event.issue.milestone?.number,
-      labels: event.issue.labels,
-      assignees: event.issue.assignees.map(({login}) => login)
-    })
-
+    const createdIssue = await apiClient.duplicateIssue(
+      event.issue,
+      event.repository
+    )
     core.info(`Issue created: ${createdIssue.url}`)
     core.debug('createdIssue:')
     core.debug(JSON.stringify(createdIssue, null, 2))
 
-    const graphqlClient = getSdk(octokit.graphql)
-    const data = await graphqlClient.projectFieldValues({
-      issueNodeId: event.issue.node_id
-    })
-    if (!(data.node && 'projectItems' in data.node)) {
-      throw new Error('Missing `projectItems` for the original issue.')
-    }
-
-    const items = data.node.projectItems.nodes ?? []
-    const projects = items
-      .filter((item): item is NonNullable<typeof items[number]> =>
-        Boolean(item)
-      )
-      .map(({project, fieldValues}) => ({
-        ...project,
-        fields: (fieldValues.nodes ?? [])
-          .map(node => {
-            if (!(node && 'field' in node)) return null
-            return {
-              id: node.field.id,
-              name: node.field.name,
-              value: ((): ProjectV2FieldValue | undefined => {
-                switch (node.__typename) {
-                  case 'ProjectV2ItemFieldDateValue':
-                    return {date: node.date}
-                  case 'ProjectV2ItemFieldIterationValue':
-                    return {iterationId: node.iterationId}
-                  case 'ProjectV2ItemFieldNumberValue':
-                    return {number: node.number}
-                  case 'ProjectV2ItemFieldSingleSelectValue':
-                    return {singleSelectOptionId: node.optionId}
-                  case 'ProjectV2ItemFieldTextValue':
-                    if (node.field.dataType === 'TEXT') return {text: node.text}
-                }
-              })()
-            }
-          })
-          .filter(
-            (
-              field
-            ): field is {
-              id: string
-              name: string
-              value: ProjectV2FieldValue
-            } => Boolean(field?.value)
-          )
-      }))
-
+    const projects = await apiClient.getProjectFieldValues(event.issue.node_id)
     for (const project of projects) {
-      const res = await graphqlClient.addIssueToProject({
-        input: {projectId: project.id, contentId: createdIssue.node_id}
-      })
+      const itemId = await apiClient.addIssueToProject(
+        createdIssue.node_id,
+        project.id
+      )
       core.info(`Added issue to project: ${project.url}`)
-      const itemId = res.addProjectV2ItemById?.item?.id
-      if (!itemId) throw new Error('Missing itemId.')
       core.debug(`itemId: ${itemId}`)
 
       for (const field of project.fields) {
-        await graphqlClient.setProjectFieldValue({
-          input: {
-            projectId: project.id,
-            itemId,
-            fieldId: field.id,
-            value: field.value
-          }
-        })
+        await apiClient.setProjectFieldValue(project.id, itemId, field)
         core.info(`- Set field value: ${field.name}`)
       }
     }
